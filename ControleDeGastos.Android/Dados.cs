095c93685668dd90f760fe32d7d97562fa1ccc97
Andre Lima <andrealveslimeira@gmail.com>
2017-11-14 23:07:08 +0100

    Gravação e restauração dos gastos em um banco SQLite com ADO.NET

--- ControleDeGastos.Android/Dados.cs
+++ ControleDeGastos.Android/Dados.cs
@@ -16,11 +16,13 @@ namespace ControleDeGastos.Android
     {
         public List<Models.Estabelecimento> Estabelecimentos { get; private set; }
         public List<Models.Gasto> Gastos { get; private set; }
+        private string _caminhoBanco;
 
         public Dados()
         {
             Estabelecimentos = new List<Models.Estabelecimento>();
             Gastos = new List<Models.Gasto>();
+            _caminhoBanco = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "gastos.db");
         }
 
         public void Carregar()
@@ -28,49 +30,111 @@ namespace ControleDeGastos.Android
             Estabelecimentos.Clear();
             Gastos.Clear();
 
-            var diretorio = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-
-            var caminhoEstabelecimentos = System.IO.Path.Combine(diretorio, "estabelecimentos.json");
-            if (System.IO.File.Exists(caminhoEstabelecimentos))
+            if (!System.IO.File.Exists(_caminhoBanco))
             {
-                using (var stream = new System.IO.FileStream(caminhoEstabelecimentos, System.IO.FileMode.Open))
-                {
-                    var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(List<Models.Estabelecimento>));
-                    Estabelecimentos = (List<Models.Estabelecimento>)serializer.ReadObject(stream);
-                }
-            }
-            else
-            {
-                for (int c = 1; c <= 10; c++)
-                {
-                    Estabelecimentos.Add(new Models.Estabelecimento() { Id = c, Nome = string.Format("Estabelecimento {0}", c) });
-                }
+                CriarBancoVazio();
             }
 
-            var caminhoGastos = System.IO.Path.Combine(diretorio, "gastos.json");
-            if (System.IO.File.Exists(caminhoGastos))
+            using (var conn = new Mono.Data.Sqlite.SqliteConnection("Data Source=" + _caminhoBanco))
             {
-                using (var stream = new System.IO.FileStream(caminhoGastos, System.IO.FileMode.Open))
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
                 {
-                    var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(List<Models.Gasto>));
-                    Gastos = (List<Models.Gasto>)serializer.ReadObject(stream);
+                    using (var comm = new Mono.Data.Sqlite.SqliteCommand(conn))
+                    {
+                        comm.CommandText = "SELECT * FROM Estabelecimento";
+                        using (var reader = comm.ExecuteReader())
+                        {
+                            using (var table = new System.Data.DataTable())
+                            {
+                                table.Load(reader);
+                                foreach (System.Data.DataRow linha in table.Rows)
+                                {
+                                    Estabelecimentos.Add(new Models.Estabelecimento()
+                                    {
+                                        Id = Convert.ToInt32(linha["Id"]),
+                                        Nome = linha["Nome"].ToString()
+                                    });
+                                }
+                            }
+                        }
+
+                        comm.CommandText = "SELECT * FROM Gasto";
+                        using (var reader = comm.ExecuteReader())
+                        {
+                            using (var table = new System.Data.DataTable())
+                            {
+                                table.Load(reader);
+                                foreach (System.Data.DataRow linha in table.Rows)
+                                {
+                                    var estabelecimentoID = Convert.ToInt32(linha["EstabelecimentoId"]);
+                                    var estabelecimento = Estabelecimentos.FirstOrDefault(e => e.Id == estabelecimentoID);
+                                    Gastos.Add(new Models.Gasto()
+                                    {
+                                        Id = Convert.ToInt32(linha["Id"]),
+                                        Data = Convert.ToDateTime(linha["Data"]),
+                                        Estabelecimento = estabelecimento,
+                                        Valor = Convert.ToDecimal(linha["Valor"])
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void CriarBancoVazio()
+        {
+            Mono.Data.Sqlite.SqliteConnection.CreateFile(_caminhoBanco);
+            using (var conn = new Mono.Data.Sqlite.SqliteConnection("Data Source=" + _caminhoBanco))
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    using (var comm = new Mono.Data.Sqlite.SqliteCommand(conn))
+                    {
+                        comm.CommandText = "CREATE TABLE Estabelecimento (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Nome TEXT NOT NULL)";
+                        comm.ExecuteNonQuery();
+
+                        for (int c = 1; c <= 10; c++)
+                        {
+                            comm.CommandText = "INSERT INTO Estabelecimento (Nome) VALUES (@Nome)";
+                            comm.Parameters.Clear();
+                            comm.Parameters.AddWithValue("@Nome", string.Format("Estabelecimento {0}", c));
+                            comm.ExecuteNonQuery();
+                        }
+
+                        comm.CommandText = "CREATE TABLE Gasto (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Data TEXT NOT NULL, EstabelecimentoId INTEGER NOT NULL, Valor REAL NOT NULL)";
+                        comm.ExecuteNonQuery();
+                    }
                 }
             }
         }
 
         public void Salvar()
         {
-            var diretorio = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            using (var conn = new Mono.Data.Sqlite.SqliteConnection("Data Source=" + _caminhoBanco))
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    using (var comm = new Mono.Data.Sqlite.SqliteCommand(conn))
+                    {
+                        comm.CommandText = "DELETE FROM Gasto";
+                        comm.ExecuteNonQuery();
 
-            using (var stream = new System.IO.FileStream(System.IO.Path.Combine(diretorio, "estabelecimentos.json"), System.IO.FileMode.Create))
-            {
-                var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(List<Models.Estabelecimento>));
-                serializer.WriteObject(stream, Estabelecimentos);
-            }
-            using (var stream = new System.IO.FileStream(System.IO.Path.Combine(diretorio, "gastos.json"), System.IO.FileMode.Create))
-            {
-                var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(List<Models.Gasto>));
-                serializer.WriteObject(stream, Gastos);
+                        comm.CommandText = "INSERT INTO Gasto (Data, EstabelecimentoId, Valor) VALUES (@Data, @EstabelecimentoID, @Valor)";
+                        foreach (var gasto in Gastos)
+                        {
+                            comm.Parameters.Clear();
+                            comm.Parameters.AddWithValue("@Data", gasto.Data);
+                            comm.Parameters.AddWithValue("@EstabelecimentoId", gasto.Estabelecimento.Id);
+                            comm.Parameters.AddWithValue("@Valor", gasto.Valor);
+                            comm.ExecuteNonQuery();
+                        }
+                    }
+                }
             }
         }
     }
